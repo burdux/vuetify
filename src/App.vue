ad8dd0146a2290a486b37411cfbcaf8061f814ed
andrei.spataru <7rl6cnfstltkiki7w2q3zic2afg2n7qddngwzquw5uuntyznj2va>
2020-08-30 16:15:38 +0300

    rework

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,103 @@
 <template>
-    <div id="app">
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+        >
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-view-dashboard</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Dashboard</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-cog</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Settings</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
-    </div>
+        <v-app-bar
+                app
+                clipped-left
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-menu
+                    left
+                    bottom
+            >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                            icon
+                            v-bind="attrs"
+                            v-on="on"
+                    >
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item
+                            v-for="n in 5"
+                            :key="n"
+                            @click="() => {}"
+                    >
+                        <v-list-item-title>Option {{ n }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+
+        <v-main>
+            <v-container
+                    class="fill-height"
+                    fluid
+            >
+
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            <span>Oracle &copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
 </template>
 
-<style>
+<script>
 
-</style>
+    export default {
+        name: 'App',
+
+        components: {
+
+        },
+
+        data: () => ({
+            drawer: false,
+        }),
+
+        created () {
+            this.$vuetify.theme.dark = true
+        },
+    };
+</script>
